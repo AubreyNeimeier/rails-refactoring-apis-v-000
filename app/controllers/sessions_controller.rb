ed3f3948b8e8e9d8c9c82a2bfbe83f47a6cbaab0
Aubrey Neimeier <neimeier.1@osu.edu>
2018-09-27 02:37:59 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,6 @@ class SessionsController < ApplicationController
     github_repo = GithubService.new
     session[:token] = github_repo.authenticate!(ENV['GITHUB_CLIENT'], ENV['GITHUB_SECRET'], params[:code])
 
-    user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
-    user_json = JSON.parse(user_response.body)
-    session[:username] = user_json["login"]
 
     redirect_to '/'
   end
